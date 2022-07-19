57ef840acdd2c6869342fc7fff3a43e444d1b637
omrajsharma <omraj7413@gmail.com>
2022-07-19 22:45:28 +0530

    session 3 completed

---
+++ js/3/script.js
@@ -0,0 +1,72 @@
+// document.getElementById("heading").innerHTML = "<b>omraj</b>";
+// document.getElementsByClassName("para")[0].innerHTML = "<b>omraj</b>";
+// document.querySelectorAll(".para")[1].innerText = "<b>omraj</b>"
+
+// document.getElementById('heading').style.color = "red";
+// document.getElementById('heading').style.color = "#ff00ee";
+// document.getElementById('heading').style.border = "1px solid black";
+// document.getElementById('heading').style.boxShadow = "0px 2px 6px #444444";
+
+// let head = document.getElementById('heading')
+// head.style.color = "red";
+// head.innerHTML = '<b>omraj</b>';
+// head.style.border = "1px solid black";
+
+// head = 'omraj'
+// head.setAttribute('align', 'center');
+// head.setAttribute('class', ' ');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Hoisting
+
+// greet();
+// console.log(a)
+
+// function greet(){
+//     console.log('Hey there');
+// }
+// var a = 10;
+
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
